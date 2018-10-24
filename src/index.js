083a1b45b4bc08d2f6ea7b6b772602564b6c724d
Sam Holloway <sgh304@nyu.edu>
2018-10-24 17:01:09 -0400

    Accepting filename as a command line argument

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ const config = require('./config.json');
 const express = require('express');
 const fs = require('fs');
 
-// Create app
-const app = express();
+// Check for filename command line argument
+const filename = process.argv[2];
+if (filename == undefined) {
+    console.log('ERROR: No filename provided as command line argument.'); 
+}
+else {
+    // Create Express app
+    const app = express();
 
-// Routes
-app.get('/', (req, res) => {
-    res.send(`Welcome to the line server.`);
-});
+    // Routes
+    app.get('/', (req, res) => {
+        res.send(`Welcome to the line server. Currently serving lines from ${filename}.`);
+    });
 
-// Listen
-app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+    // Listen
+    app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+}
