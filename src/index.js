8fd30b1d50f61db3285452f01c01ee05dd97cea7
Sam Holloway <sgh304@nyu.edu>
2018-10-24 18:19:25 -0400

    Added preprocessing of the text file, and some experimentation and documentation to justify the method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const config = require('./config.json');
 const express = require('express');
 const fs = require('fs');
+const split = require('split');
 
 // Check for filename command line argument
 const filename = process.argv[2];
@@ -9,14 +10,39 @@ if (filename == undefined) {
     console.log('ERROR: No filename provided as command line argument.'); 
 }
 else {
-    // Create Express app
-    const app = express();
+    // Perform preprocessing. 
+    /* The basic idea is to build a map of line numbers to the bytes that they start on in the file.
+    To avoid using too much memory, we don't record the starting byte of every line in the file,
+    however even with just a few records we can speed up serving lines a lot, since, when a line is
+    requested, we can simply find the closest prior line number on record, navigate to its starting byte,
+    and read forward until we reach the requested line. This makes the maximum number of bytes read for
+    any given request roughly equal to the number of bytes we wait between recording line-starting bytes
+    in this step, which is defined as "lineBytesDistance" in the config file (default 1,000,000 bytes).
+    You can read more about my decisions regarding this default value in the readme and
+    src/test/timing.js. */
+    const lineBytes = [{line: 0, byte: 0}];
+    let line = 0;
+    let bytesSinceLastRecord = 0;
+    let stream = fs.createReadStream(filename)
+                        .pipe(split())
+                        .on('data', (text) => {
+                            if (bytesSinceLastRecord >= config.lineBytesDistance) {
+                                lineBytes.push({line: line, byte: lineBytes[lineBytes.length - 1].byte + bytesSinceLastRecord});
+                                bytesSinceLastRecord = 0;
+                            }
+                            line += 1;
+                            bytesSinceLastRecord += text.length;
+                        })
+                        .on('close', () => {
+                            // Create Express app
+                            const app = express();
 
-    // Routes
-    app.get('/', (req, res) => {
-        res.send(`Welcome to the line server. Currently serving lines from ${filename}.`);
-    });
+                            // Routes
+                            app.get('/', (req, res) => {
+                                res.send(`Welcome to the line server. Currently serving lines from ${filename}.`);
+                            });
 
-    // Listen
-    app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+                            // Listen
+                            app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+                        });
 }
---
+++ src/test/timing.js
@@ -0,0 +1,38 @@
+/* This file contains some simple experiments I ran to measure the feasibility and performance of preprocessing
+a text file to create a map of some of its line numbers to the bytes that those lines start on. */
+
+// Imports
+const fs = require('fs');
+const split = require('split');
+
+function timeReadingBytes(filename, bytes) {
+    /* This logs the time in seconds that it takes to read a number of bytes from a text file. I wanted to
+    know this to help decide about how many bytes to allow in between the records in my map of line numbers
+    to starting bytes, since, in theory, the worst-case line lookup woud have to read that many bytes from
+    the file before serving a line to the user.
+    With several trials, I determined that putting ~1,000,000 bytes between records would be a good choice,
+    since reading ~1,000,000 bytes tended to take at the most 20ms, not a bad worst-case for a server response.
+    Also, a map with ~1,000,000 bytes between records would only take up ~200KB for a 10GB file (assuming each
+    Number, line number and byte, takes up about 10 bytes, 10GB / 1000000B * 10B * 2 = 200KB), and ~2MB for a
+    100GB file (100GB / 1000000B * 10B * 2 = 2MB), meaning I could comfortably keep the map in memory even when
+    serving quite large files.
+    During these experiments, I also learned that reading more bytes through a stream piped into the split package
+    doesn't necessarily increase read time linearly. For example, while reading 1,000,000B took ~20ms, reading
+    10,000,000B usually only took ~65ms, and even 1GB only took ~4.5s. This suggests that my preprocessing
+    could be quite fast, even for large files. */
+    let start = Date.now();
+    let read = 0;
+    let done = false;
+    let stream = fs.createReadStream(filename)
+                        .pipe(split())
+                        .on('data', (text) => {
+                            read += text.length;
+                            if (read >= bytes && done == false) {
+                                console.log((Date.now() - start) / 1000);
+                                done = true;
+                                stream.destroy();
+                            }
+                        });
+}
+
+//timeReadingBytes('testfile4.txt', 1000000);
