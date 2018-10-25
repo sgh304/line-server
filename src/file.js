93267d7d3a4460ca75bb7851dc61934cdff18550
Sam Holloway <sgh304@nyu.edu>
2018-10-25 11:43:41 -0400

    Simplified the file handling API a lot, allowing me to clean up index.js as well

--- src/file.js
+++ src/file.js
@@ -1,28 +1,98 @@
-/* This file contains helper functions for handling the text file to be served. */
+/* This file contains the FileHandler class, a class that groups relevant data and vital methods for
+reading the text file that the server is... serving. */
 
 // Imports
 const fs = require('fs');
 const split = require('split');
 
-function getSplitStream(filename, start, textCallback) {
-    /* Returns a Promise that resolves at the end of a ReadStream of the file with the
-    given filename, starting at the given byte. The stream calls the given textCallback
-    on every line of the stream. If the file cannot be read, an error message is logged
-    and the process is terminated. */
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(filename, {start: start})
-            .pipe(split())
-            .on('data', (text) => {
-                textCallback(text);
-            })
-            .on('end', () => {
+class FileHandler {
+    constructor(filename, lineBytesDistance) {
+        /* Creates a FileHandler object that handles the file with the given filename and
+        observes the given lineBytesDistance. */
+        this.filename = filename;
+        this.lineBytesDistance = lineBytesDistance;
+        this.lineBytes = [{line: 0, byte: 0}]; // Objects that map line numbers to the lines' starting bytes
+    }
+
+    preprocess() {
+        /* Returns a Promise that resolves after performing the initial processing of the handled
+        text file. The lineBytes array is built, mapping some of the handled file's line numbers to the
+        lines' starting bytes, with the distance between each record being roughly equal to the handler's
+        lineBytesDistance. */
+        return new Promise((resolve, reject) => {
+            let currentLine = 0;
+            let currentByte = 0;
+            this.readLines(0, (text) => {
+                /* When the difference between the current byte and the last recorded line's starting byte exceeds
+                the handler's lineBytesDistance, a new record is added. */
+                if (currentByte - this.lineBytes[this.lineBytes.length - 1].byte >= this.lineBytesDistance) {
+                    this.lineBytes.push({line: currentLine, byte: currentByte});
+                }
+                // Increment line number and read bytes each line.
+                currentLine += 1;
+                currentByte += text.length + 2;
+            }).then(() => {
+                this.totalLines = currentLine - 1;
                 resolve();
-            })
-            .on('error', () => {
-                console.log(`ERROR: Could not read file ${filename}!`);
-                process.exit();
-            })
-    })
+            });
+        })
+    }
+
+    getLine(line) {
+        /* Returns a Promise that resolves with the requested line from the handled text file. The
+        line is retrieved by locating the record in lineBytes that is nearest and prior to the requested
+        line, then reading the text file starting at that record's byte position until the requested line
+        is reached. */
+        return new Promise((resolve, reject) => {
+            // Locate the nearest and prior lineByte.
+            let lineByte = 0;
+            for (let i = this.lineBytes.length - 1; i >= 0; i--) {
+                if (this.lineBytes[i].line < line) {
+                    lineByte = this.lineBytes[i];
+                    break;
+                }
+            }
+            // Read forward to requested line.
+            let currentLine = lineByte.line;
+            let lineText = '';
+            this.readLines(lineByte.byte, (text) => {
+                // TODO: Stop reading after line is found
+                if (currentLine == line) {
+                    lineText = text;
+                }
+                currentLine += 1;
+            }).then(() => { resolve(lineText); });
+        });
+    }
+
+    readLines(start, textCallback) {
+        /* Returns a Promise that resolves at the end of a ReadStream of the file with the
+        given filename, starting at the given byte. The stream calls the given textCallback
+        on every line of the stream. If the file cannot be read, an error message is logged
+        and the process is terminated. */
+        return new Promise((resolve, reject) => {
+            fs.createReadStream(this.filename, {start: start})
+                .pipe(split())
+                .on('data', (text) => {
+                    textCallback(text);
+                })
+                .on('end', () => {
+                    resolve();
+                })
+                .on('error', () => {
+                    this.terminate();
+                });
+        });
+    }
+
+    terminate() {
+        /* A helper function that logs an error message and immediately terminates the process,
+        to be used when there is some sort of error reading the handled file. */
+        console.log(`ERROR: Could not read file ${this.filename}!`);
+        process.exit();
+    }
+
 }
 
-module.exports = {getSplitStream: getSplitStream};
+
+module.exports = {FileHandler: FileHandler};
--- src/index.js
+++ src/index.js
@@ -3,62 +3,36 @@ const config = require('./config.json');
 const express = require('express');
 const file = require('./file.js');
 
-// Check for filename command line argument
-const filename = process.argv[2];
-if (filename == undefined) {
-    console.log('ERROR: No filename provided as command line argument.'); 
-}
-else {
-    // Perform preprocessing. 
-    /* The basic idea is to build a map of line numbers to the bytes that they start on in the file.
-    To avoid using too much memory, we don't record the starting byte of every line in the file,
-    however, even with just a few records we can speed up serving lines a lot, since, when a line is
-    requested, we can simply find the closest prior line number on record, navigate to its starting byte,
-    and read forward until we reach the requested line. This makes the maximum number of bytes read for
-    any given request roughly equal to the number of bytes we wait between recording line-starting bytes
-    in this step, which is defined as "lineBytesDistance" in the config file (default 1,000,000 bytes).
-    You can read more about my decisions regarding this default value in the readme and src/test/timing.js. */
-    const lineBytes = [{line: 0, byte: 0}]; // Objects that map line numbers to the first byte of the lines
-    let line = 0; // The current line number
-    let byte = 0; // The current number of bytes read
-    file.getSplitStream(filename, 0, (text) => {
-        /* When the difference between the current byte and the last recorded line's starting byte exceeds
-        the difference specified in the config file, a new record is added. */
-        if (byte - lineBytes[lineBytes.length - 1].byte >= config.lineBytesDistance) {
-            lineBytes.push({line: line, byte: byte});
-        }
-        line += 1;
-        byte += text.length + 2;
-    }).then(() => {
-        // Create Express app
-        const app = express();
-
-        // Routes
-        app.get('/', (req, res) => {
-            res.send(`Welcome to the line server. Currently serving lines from ${filename}.`);
-        });
-
-        app.get('/lines/:line/', (req, res) => {
-            // Get the closest prior lineByte record
-            let lineByte = 0;
-            for (let i = lineBytes.length - 1; i >= 0; i--) {
-                if (lineBytes[i].line < req.params.line) {
-                    lineByte = lineBytes[i];
-                    break;
-                }
-            }
-            // Read forward to requested line
-            let line = lineByte.line;
-            let byte = lineByte.byte;
-            file.getSplitStream(filename, byte, (text) => {
-                if (line == req.params.line) {
-                    res.send(text);
-                }
-                line += 1;
-            })
-        })
-
-        // Listen
-        app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+// Create the file handler using the filename from command line arguments and the lineByteDistance from the config file.
+const fileHandler = new file.FileHandler(process.argv[2], config.lineByteDistance);
+fileHandler.preprocess().then(() => {
+    // Create the Express app
+    const app = express();
+    // Define the app's routes
+    app.get('/', (req, res) => {
+        res.send(`Welcome to Sam's line server. Currently serving lines from ${fileHandlers.filename}.`);
     });
-}
+    // TODO: Fix line count bug. It's either over-counting or the last 2 lines are inaccessible.
+    app.get('/lines/', (req, res) => {
+        res.send(`${fileHandler.filename} contains ${fileHandler.totalLines} lines.`);
+    });
+    app.get('/lines/:line/', (req, res) => {
+        const line = parseInt(req.params.line);
+        // Bad Request
+        if (line == undefined) {
+            res.sendStatus(400);
+        }
+        // Payload Too Large
+        else if (line > fileHandler.totalLines) {
+            res.sendStatus(413);
+        }
+        // Valid Request
+        else {
+            fileHandler.getLine(line).then((text) => {
+                res.send(text);
+            });
+        }
+    });
+    // Listen on the port defined in the config file
+    app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+});
